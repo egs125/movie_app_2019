82c3dc4422dd88170cf84179eb293f86a7a367b2
egs125 <egs125@gmail.com>
2019-08-07 22:02:30 +0900

    2-1 components study

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Food (props){
+  return <h1>I like {props.fav}</h1>;
+}
+/* 아래와 동일
+function Food( {fav} ){
+  return <h1>I Like {fav}</h1>
+}
+*/
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="bab" />
+      <Food fav="gogi" />
+      <Food fav="ramyun" />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+//component = html 반환
+//react는 component 를 사용해서 html처럼 작성하는 경우에 필요
+//jsx = javascript + html
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
