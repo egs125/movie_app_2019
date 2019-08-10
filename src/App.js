5bda7a9f3c633d87c6fe056de45375aec8b0221c
egs125 <egs125@gmail.com>
2019-08-10 22:51:37 +0900

    2-3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+//props -> arguements -> props 내부에서 fav 가져옴
+/*
 function Food (props){
   return <h1>I like {props.fav}</h1>;
 }
-/* 아래와 동일
+ 아래와 동일
 function Food( {fav} ){
   return <h1>I Like {fav}</h1>
 }
 */
+function Food ({name, picture}){
+  return (
+    <div>
+      <h1>I Like {name}</h1>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id : 1,
+    name : "kimchi",
+    image : "https://images.app.goo.gl/vdCjQvgJahySrHRPA"
+  },
+  {
+    id : 2,
+    name : "gogi",
+    image : "https://images.app.goo.gl/AiJPPbAy3s4ehZDX8"
+  },
+  {
+    id : 3,
+    name : "ramyun",
+    image : "https://images.app.goo.gl/pppEM5PgwPrH9fCX8"
+  }
+
+];
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="bab" />
-      <Food fav="gogi" />
-      <Food fav="ramyun" />
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
