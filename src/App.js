c88e7fde93dd8d3b458174bf72685e47dff016e7
egs125 <egs125@gmail.com>
2019-08-17 18:07:30 +0900

    setState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+class App extends Component {
+  state = {
+    count : 0
+  };
+
+  add = () => {
+    this.setState(current => ({ count : current.count + 1}));
+    //this.setState({count : this.state.count + 1});
+  };
+  minus = () => {
+    this.setState(current => ({ count : current.count - 1}));
+    //this.setState({count : this.state.count -1 });
+  };
+
+  render(){
+    return (
+    <div>
+      <h1>the number is {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>);
+  }
+}
+
 
 //props -> arguements -> props 내부에서 fav 가져옴
 /*
@@ -9,7 +35,7 @@ function Food (props){
 function Food( {fav} ){
   return <h1>I Like {fav}</h1>
 }
-*/
+
 function Food ({name, picture}){
   return (
     <div>
@@ -19,34 +45,19 @@ function Food ({name, picture}){
   );
 }
 
+Food.PropTypes - {
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired
+};
+
 const foodILike = [
   {
     id : 1,
     name : "kimchi",
     image : "https://images.app.goo.gl/vdCjQvgJahySrHRPA"
   },
-  {
-    id : 2,
-    name : "gogi",
-    image : "https://images.app.goo.gl/AiJPPbAy3s4ehZDX8"
-  },
-  {
-    id : 3,
-    name : "ramyun",
-    image : "https://images.app.goo.gl/pppEM5PgwPrH9fCX8"
-  }
-
 ];
+*/
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
-      ))}
-    </div>
-  );
-}
 
 export default App;
