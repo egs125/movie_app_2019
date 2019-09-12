5218280fb2b8d004a3e07f45afdf8a7358868ba2
egs125 <egs125@gmail.com>
2019-09-12 22:06:43 +0900

    style css

--- src/App.js
+++ src/App.js
@@ -1,65 +1,52 @@
-import React, { Component } from "react";
-import PropTypes from 'prop-types';
+import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
   state = {
-    isLoading : true,
-    movies : []
+    isLoading: true,
+    movies: []
   };
-
   getMovies = async () => {
-    const { data : { data : { movies }}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    console.log( movies);
-    this.setState({ movies, isLoading : false});
-  }
-
-  componentDidMount(){
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
     this.getMovies();
   }
-
-  render(){
+  render() {
     const { isLoading, movies } = this.state;
-    return <div>{isLoading ? "Loading" : movies.map(movies => {
-      return < Movie id={movies.id} year={movies.year} title={movies.title} summary={movies.summary} poster={movies.medium_cover_image} />
-    })}</div>;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
-
-
-//props -> arguements -> props 내부에서 fav 가져옴
-/*
-function Food (props){
-  return <h1>I like {props.fav}</h1>;
-}
- 아래와 동일
-function Food( {fav} ){
-  return <h1>I Like {fav}</h1>
-}
-
-function Food ({name, picture}){
-  return (
-    <div>
-      <h1>I Like {name}</h1>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-Food.PropTypes - {
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired
-};
-
-const foodILike = [
-  {
-    id : 1,
-    name : "kimchi",
-    image : "https://images.app.goo.gl/vdCjQvgJahySrHRPA"
-  },
-];
-*/
-
 
 export default App;
